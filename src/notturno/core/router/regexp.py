4708e0bff0e4eb59926797b1d1c4b109bced1f04
AmaseCocoa <cocoa@amase.cc>
2025-02-02 01:59:31 +0900

    feat: Use re when re2 is not available

--- src/notturno/core/router/regexp.py
+++ src/notturno/core/router/regexp.py
@@ -1,4 +1,8 @@
-import re2
+try:
+    import re2 as re
+except ModuleNotFoundError:
+    import re
+
 
 class RegExpRouter:
     def __init__(self, root_path: str = ""):
@@ -11,10 +15,8 @@ class RegExpRouter:
         if pattern == "/" or pattern == "":
             regex_pattern = r"^/$"
         else:
-            regex_pattern = re2.sub(
-                r":(\w+)", r"(?P<\1>[^/]+)", pattern
-            )
-            regex_pattern = re2.sub(r"\*", r"(.+)", regex_pattern)
+            regex_pattern = re.sub(r":(\w+)", r"(?P<\1>[^/]+)", pattern)
+            regex_pattern = re.sub(r"\*", r"(.+)", regex_pattern)
 
         if method not in self.routes:
             self.routes[method] = []
@@ -27,32 +29,31 @@ class RegExpRouter:
             f"(?P<route_{index}_{method}>{pattern})"
             for index, (pattern, _) in enumerate(self.routes[method])
         )
-        self.compiled_regex[method] = re2.compile(combined_pattern)
+        self.compiled_regex[method] = re.compile(combined_pattern)
 
     def match(self, path):
-        result = {}
-        for method, patterns in self.routes.items():
-            if method not in self.compiled_regex:
-                continue
-
-            match = self.compiled_regex[method].match(path)
-            if match:
-                for index, (pattern, handler) in enumerate(patterns):
-                    if match.group(f"route_{index}_{method}"):
-                        params = {
-                            key: match.group(key)
-                            for key in match.groupdict()
-                            if key
-                            not in [f"route_{i}_{method}" for i in range(len(patterns))]
-                        }
-                        if not params:
-                            params = {}
-                        result[method] = {
-                            "func": handler,
-                            "params": params,
-                        }
-            else:
-                result[method] = None
+        result = {
+            method: (
+                {
+                    "func": handler,
+                    "params": {
+                        key: match.group(key)
+                        for key in match.groupdict()
+                        if key
+                        and key
+                        not in [f"route_{i}_{method}" for i in range(len(patterns))]
+                    },
+                }
+            )
+            if (match := self.compiled_regex[method].match(path))
+            and any(
+                (match.group(f"route_{index}_{method}") and (handler := handler_value))
+                for index, (pattern, handler_value) in enumerate(patterns)
+            )
+            else None
+            for method, patterns in self.routes.items()
+            if method in self.compiled_regex
+        } or None
         return result if result else None
 
     def combine(self, other_router):
