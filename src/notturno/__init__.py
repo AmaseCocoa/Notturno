fadf8b0c8bc7c1f5d1711a60153f8ef20a263253
AmaseCocoa <cocoa@amase.cc>
2025-02-16 04:19:59 +0900

    feat: __version__

--- src/notturno/__init__.py
+++ src/notturno/__init__.py
@@ -1,8 +1,10 @@
+
 from .app import Notturno
 from .gear import Gear
 from .models.request import Request
 from .models.response import Response
 from .models.websocket import WebSocket
 from .middleware import BaseMiddleware
+from .lib import __version__
 
-__all__ = ["Notturno", "Gear", "Request", "Response", "WebSocket", "BaseMiddleware"]
+__all__ = ["Notturno", "Gear", "Request", "Response", "WebSocket", "BaseMiddleware", "__version__"]
