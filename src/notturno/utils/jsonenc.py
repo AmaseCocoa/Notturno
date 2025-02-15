f92c64e51139e6b15a0daa05ccd9f543ba767216
AmaseCocoa <cocoa@amase.cc>
2025-02-16 02:48:21 +0900

    feat: Common error classes

--- src/notturno/utils/jsonenc.py
+++ src/notturno/utils/jsonenc.py
@@ -4,12 +4,30 @@ except ModuleNotFoundError:
     orjson = None
     import json
 
+class JSONError(Exception):
+    pass
+
+class JSONDecodeError(JSONError):
+    pass
+
+class JSONEncodeError(JSONError):
+    pass
+
 def dumps(obj: dict) -> bytes:
     if orjson:
-        return orjson.dumps(obj)
+        try:
+            return orjson.dumps(obj)
+        except orjson.JSONEncodeError as e:
+            raise JSONEncodeError(*e.args)
     return json.dumps(obj).encode()
 
 def loads(obj: str | bytes | bytearray) -> bytes:
     if orjson:
-        return orjson.loads(obj)
-    return json.loads(obj)
+        try:
+            return orjson.loads(obj)
+        except orjson.JSONDecodeError as e:
+            raise JSONDecodeError(*e.args)
+    try:
+        return json.loads(obj)
+    except json.JSONDecodeError as e:
+        raise JSONDecodeError(*e.args)
