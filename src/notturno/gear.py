74d00f3d72b5b9193e8fe13c2f7aa15fa53bd01a
AmaseCocoa <cocoa@amase.cc>
2025-02-16 04:20:10 +0900

    chore: remove anyio

--- src/notturno/gear.py
+++ src/notturno/gear.py
@@ -3,7 +3,7 @@ from .core.router.regexp import RegExpRouter
 
 
 class Gear(Notturno):
-    def __init__(self, root_path: str = "", async_backend="asyncio"):
-        super().__init__(async_backend)
+    def __init__(self, root_path: str = ""):
+        super().__init__()
         self._router = RegExpRouter(root_path=root_path)
         self._internal_router = RegExpRouter(root_path=root_path)
