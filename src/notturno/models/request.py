e3fedaaf8fd516d37bc1f32f3d02a9fd206eb7dc
AmaseCocoa <cocoa@amase.cc>
2025-02-16 02:48:34 +0900

    feat: asgi to request support

--- src/notturno/models/request.py
+++ src/notturno/models/request.py
@@ -1,17 +1,43 @@
 from typing import Any, Dict, Optional
 
-from pydantic import BaseModel, Field
+from yarl import URL
 
-from ..utils.url import URL
+from ..utils.jsonenc import loads
+from ..utils.multipart import parse_multipart
+from ..utils.query import parse_qs
 
-
-class Request(BaseModel):
-    method: str = Field(description="HTTP Method")
-    url: URL = Field(description="Request URL")
-    headers: Optional[Dict[str, str]] = Field(
-        default_factory=dict, description="Request Header"
+async def from_asgi(scope: Dict[str, Any], receive: Any):
+    headers = {
+        key.decode("utf-8"): value.decode("utf-8")
+        for key, value in scope["headers"]
+    }
+    response_body = b""
+    while True:
+        message = await receive()
+        response_body += message.get("body", b"")
+        if not message.get("more_body", False):
+            break
+    if scope["method"].lower() == "POST" or scope["method"].lower() == "PUT":
+        if 'multipart/form-data' in headers["content-type"]:
+            response_body = await parse_multipart(scope, receive)
+        else:
+            response_body = response_body.decode("utf-8")
+    else:
+        response_body = response_body.decode("utf-8")
+    return Request(
+        method=scope["method"].upper(),
+        url=URL(f"{scope['scheme']}://{headers['host']}{scope['path']}"),
+        headers=headers,
+        query=parse_qs(scope["query_string"]),
+        body=response_body,
     )
-    query: Optional[Dict[str, Any]] = Field(
-        default_factory=dict, description="Query Params"
-    )
-    body: Optional[Any] = Field(default=None, description="Request Body")
+
+class Request:
+    def __init__(self, method: str, url: str | URL, headers: Dict[str, str]={}, query: Dict[str, str]={}, body: Any=""):
+        self.method: str = method
+        self.url: URL = url if isinstance(url, URL) else URL(url)
+        self.headers: Optional[Dict[str, str]] = headers
+        self.query: Optional[Dict[str, Any]] = query
+        self.body: Optional[Any] = body
+
+    def json(self) -> dict: return loads(self.body).decode("utf-8")
